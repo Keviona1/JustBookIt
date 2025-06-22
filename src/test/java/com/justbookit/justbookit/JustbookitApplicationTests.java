7484c67a89a79c55763d54a2310f95df5822eaa4
kev <kevionasaliasi14@hotmail.com>
2025-06-22 11:59:25 +0200

    Add Spring Boot backend structure and initial entities

---
+++ src/test/java/com/justbookit/justbookit/JustbookitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justbookit.justbookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustbookitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
